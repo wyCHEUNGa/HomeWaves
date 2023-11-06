6ae698aa3b126330c554f47ab953592d89ec2da1
wyCHEUNGa <61044800+wyCHEUNGa@users.noreply.github.com>
2023-11-06 15:43:08 +0800

    Review IMF Method

--- app.py
+++ app.py
@@ -116,6 +116,97 @@ def peak_detected(b_l, low, high, fd):
     
     return temp
 
+def peak_detectedv2(b_l, low, high, fd):
+    temp=[]
+
+    if fd == 1:
+        temp[:141] = np.zeros(141)
+
+    else:
+    #{'1':low,'2':high-1,'3':fd,'4':"add results of sliding window"}
+
+        # for h in range(0,40):
+        for h in range(0,512):
+            with open("./data/b_l_range"+str(low)+"_to_"+str(high-1)+".txt"    , "a") as branch_file:
+
+                for w in range(0,141):
+                    #TESTER w=5; h=20
+
+                    if (h%5<2):
+                        temp.append(0)
+
+                    if (h%5==2):
+                        # if (w<(135+2)):
+                        if (w<(135+2)):
+                            if (w%9<4):
+                                temp.append(0)
+                            if (w%9==4):
+                                ### Starting Peak Detection. Creation of 9x5 sliding window. 
+                                th_motion = (
+                                b_l[w-4][h-2]+
+                                b_l[w-3][h-2]+
+                                b_l[w-4][h-1]+
+                                b_l[w-3][h-1]+
+                                b_l[w-4][h-0]+
+                                b_l[w-3][h-0]+
+                                b_l[w-4][h+1]+
+                                b_l[w-3][h+1]+
+                                b_l[w-4][h+2]+
+                                b_l[w-3][h+2]+
+
+                                b_l[w-2][h-2]+
+                                b_l[w-1][h-2]+
+                                b_l[w-0][h-2]+
+                                b_l[w+1][h-2]+
+                                b_l[w+2][h-2]+
+
+                                b_l[w+4][h-2]+
+                                b_l[w+3][h-2]+
+                                b_l[w+4][h-1]+
+                                b_l[w+3][h-1]+
+                                b_l[w+4][h-0]+
+                                b_l[w+3][h-0]+
+                                b_l[w+4][h+1]+
+                                b_l[w+3][h+1]+
+                                b_l[w+4][h+2]+
+                                b_l[w+3][h+2]+
+
+                                b_l[w-2][h+2]+
+                                b_l[w-1][h+2]+
+                                b_l[w-0][h+2]+
+                                b_l[w+1][h+2]+
+                                b_l[w+2][h+2])/30
+
+                                #print("b_l["    ,w,   "]["   ,h,   "] is ",
+                                #      np.round(b_l[w][h],6), " \t 1.5*noise threshold is ", np.round(1.5*th_motion,6))
+
+                                branch_file.write("b_l["  +  str(w)  +  "]["  +  str(h)  +  "] is "  +
+                                    str(np.round(b_l[w][h],6))  +  " \t 1.5*noise threshold is "  +  str(np.round(1.5*th_motion,6))+"\n")
+                                if b_l[w][h]>=1.5*th_motion:
+                                    branch_file.write("Yes human detected.\n")
+                                    temp.append(1)
+                                else:
+                                    branch_file.write("Move to next.\n")
+                                    temp.append(0)
+                            if (w%9>4):
+                                temp.append(0)
+                        else:#(w is 137 138 139 140)
+                                temp.append(0)
+                        
+
+                    if (h%5>2):
+                        temp.append(0)
+                    #else:
+                        #temp.append(0)
+
+
+
+        
+    update_logs091023(low, high, fd, "./data/logs"+DDMM+"23.txt")
+    
+    return temp
+
+
 def image_show(image,caption):
     col1, col2, col3 = st.columns(3)
 
@@ -135,7 +226,7 @@ st.set_page_config(
 )
 
 # read csv from a github repo
-#dataset_url = "https://raw.githubusercontent.com/wyCHEUNGa/HomeWaves/main/1rename21.csv"
+#dataset_url = "https://raw.githubusercontent.com/wyCHEUNGa/HomeWaves/main/raw14.csv"
 dataset_url = "https://raw.githubusercontent.com/wyCHEUNGa/HomeWaves/main/test2.csv"
 #df_RO = pd.read_csv("https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv")
 
@@ -146,7 +237,7 @@ def get_data() -> pd.DataFrame:
 #df = get_data()
 df = pd.read_csv(dataset_url, header=None)
 
-DDMM = "1510"  ### Update Correct Today's Date ###
+DDMM = "0311"  ### Update Correct Today's Date ###
 TP01 = "Program Loaded"
 TP02 = "Initialisaton of Pandas DataFrame completed."
 TP03 = "Demo Code Starts Here"
@@ -162,12 +253,13 @@ try:
     f = open("./data/logs"+DDMM+"23.txt","x")
     f.write("Computer Time is" + str(now) + "\n")
     f.close()
+    now = datetime.datetime.now()
 except OSError as e:
     print(TP12, " Try deleting files in ./data folder and refresh programme")
     print("\n\nReady, Set, Go!")
     exit()
 else:
-    print(TP01)
+    print(TP01," at ",now)
 
 ### Prepare All My Test Buffers ####Tester:      #df_141complex.shape  #series_141
 df_141i = pd.DataFrame(index=range(len(df.index)),columns=range(141))
@@ -181,7 +273,7 @@ for v in range(141):
     df_141q[v] = df[v+141]
 for v in range(141):
     df_141complex[v] = np.sqrt(   np.power(df_141i[v],2) +  np.power(df_141q[v],2)   )
-print(70)
+
 #Transposed for signal processing
 dataT=df_141complex.transpose()
 ################################################################################################################################################
@@ -189,14 +281,15 @@ dataT=df_141complex.transpose()
 ### Prior starting signal processing, the order of windows is chosen as 26 and 50 resp.
 rcParams['figure.figsize'] = 5.7,3.27
 #Window shape Hamming, while the LPF filter
-# freqy above 250Hz
+# freqy above 250Hz/1000 0.25
 numtaps = 26 #page 70:09
-f2 = 250/1000 #end of notch
-h = signal.firwin(numtaps+1, f2*2,pass_zero='lowpass',window='hamming')
+f2 = 0.7/500
+h = signal.firwin(numtaps+1, f2,pass_zero='lowpass',window='hamming')
+#h = signal.firwin(numtaps+1, f2*2,pass_zero='lowpass',window='hamming')
 #plt.stem(fftpack.fftfreq(h.size),     np.abs(fftpack.fft(h)),  linefmt ='--', markerfmt ='')
 
 numtaps = 50 #page 70:09
-f2 = 100/1000
+f2 = 0.7/1000
 h50 = signal.firwin(numtaps+1, f2*2)
 #plt.xlim(0,0.6)
 
@@ -207,8 +300,8 @@ array1=np.zeros(s, dtype=complex)
 
 array2=np.zeros(s, dtype=complex)
 
-#M = 50
-#coef = np.hamming(M+1)
+M = 50
+coef = np.hamming(M+1)
 #coef = signal.windows.hamming(M+1)
 start_dist_of_interest = 0
 
@@ -228,14 +321,14 @@ for i in range(0,len(df_141complex[0])):
     
     ######################
     #Cascaded a second filter
-    #dataT_sfil= np.convolve(fftpack.ifft(temp),coef,mode='same')           #add into 2D arr
-    dataT_sfil = signal.lfilter(h50,1,filtered_24b)
-    topr=np.copy(dataT_sfil[:25])
-    for i in range(0,141):
-        if i<116: #0 to 115
-            dataT_sfil[i]=dataT_sfil[i+24]
-        else:     #116 117 ... 131 132 133 4 5 6 7 8 9 140
-            dataT_sfil[i]=topr[i-116]
+    dataT_sfil= np.convolve(fftpack.ifft(temp),coef,mode='same')           #add into 2D arr
+    # dataT_sfil = signal.lfilter(h50,1,filtered_24b)
+    # topr=np.copy(dataT_sfil[:25])
+    # for i in range(0,141):
+    #     if i<116: #0 to 115
+    #         dataT_sfil[i]=dataT_sfil[i+24]
+    #     else:     #116 117 ... 131 132 133 4 5 6 7 8 9 140
+    #         dataT_sfil[i]=topr[i-116]
     
     array1[start_dist_of_interest]=filtered_24b
     array2[start_dist_of_interest]=dataT_sfil
@@ -271,25 +364,25 @@ print("Done Clutter Shift, 2nd heatmap in line will be displayed.")
 #output b_s and b and comparision statement
 ###Total: 1131 | Strategically Take x[0-9][0] | Take x[0-9][1] | ...| Take x[0-9][140]
 ###Created by the above strategic, a 9x141 short observation. There r 4 short in 1 long observ
-s = (141,10); b_s=np.zeros(s)
-s = (141,40); b_l=np.zeros(s)
-s = (10); tem=np.zeros(s)
-s = (40); longobsvtem=np.zeros(s)
+s = (141,128); b_s=np.zeros(s)
+s = (141,512); b_l=np.zeros(s)
+s = (128); tem=np.zeros(s)
+s = (512); longobsvtem=np.zeros(s)
 c = 1.2 #page 70:10
-cnt = 71
+cnt = 0
 
 #Update fastdetected database, How to use:
 #Mark the binary outcome of fast movement >Take the 'low' indices > Put
 #inside a queue >This in turn will be used by PEAK DETECTION
 fastdetected = 0
 
-while(cnt<np.floor(len(df_141complex[0])/40)):
+while(cnt<np.floor(len(df_141complex[0])/512)):
 
     ####For the 1st b_l block... get b_l block for [0-40)
-    low=40*cnt; high=low+40 #40 will be exclusive
-    ptr = np.random.randint(low,high-9)            #randomise any 10 data for bs. Param 2 exclusive.
+    low=512*cnt; high=low+512 #512 will be exclusive
+    ptr = np.random.randint(low,high-127)            #randomise any 10 data for bs. Param 2 exclusive.
     for j in range(0,141):
-        for i in range(ptr,ptr+10):      #any 10 between [0-30] 30 incluc.
+        for i in range(ptr,ptr+128):      #any 100 between [0-30] 30 incluc.
             
             tem[i-ptr]=clutter_suppr[i][j]      #clutter_suppr[10][0] to clutter_suppr[19][0]
         
@@ -300,7 +393,7 @@ while(cnt<np.floor(len(df_141complex[0])/40)):
     ########################################################
 
     #For 1x single b_l block, finding out FFT is useable or not useable
-    signalSTR = np.max(b_s)/(np.sum(b_s)/(10*141))
+    signalSTR = np.max(b_s)/(np.sum(b_s)/(128*141))
 
     for j in range(0,141):
         for i in range(low,high):      #  [40-80)
@@ -312,8 +405,8 @@ while(cnt<np.floor(len(df_141complex[0])/40)):
                                                     #i = [1120:~not computerised]
         
         b_l[j][:]=np.abs(fftpack.fft(longobsvtem))
-        
-    signalSTRL = np.max(b_l)/(np.sum(b_l)/(40*141))
+    
+    signalSTRL = np.max(b_l)/(np.sum(b_l)/(512*141))
     if signalSTR>(c*signalSTRL):
         print(signalSTR,signalSTRL*c,'To Be Discarded.') #page 70:10
         fastdetected = 1
@@ -326,7 +419,7 @@ while(cnt<np.floor(len(df_141complex[0])/40)):
     
     #Continued, so if FFT is useable, find peak amplitude
 
-    flags_if = peak_detected(b_l, low, high, fastdetected)
+    flags_if = peak_detectedv2(b_l, low, high, fastdetected)
     for i in range(len(flags_if)):
         if i%141 == 140:
             print(flags_if[i])
